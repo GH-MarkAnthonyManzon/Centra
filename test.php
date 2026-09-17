baa1483a6308b2d94411245a8ef6771aa6e2aec3
GH-MarkAnthonyManzon <manzonmark.ph@gmail.com>
2026-09-17 09:32:41 +0800

    Remove width overflow that causes horizontal scrolling in the home page

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    $name = "Mark Anthony";
-    $age = 27;
-    $gender = "Male";
-
-    echo("Name: " . $name . "\nAge: " . $age . "\nGender: " . $gender);
-    echo "<h1>Hello" . " My name is " . $name . "</h1>";
-?>
